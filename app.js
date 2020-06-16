dc5de4babc3720f73363b1686564129032b0b45d
david <david.c.quinn25>
2020-06-16 15:52:59 -0700

    fix bug

--- app.js
+++ app.js
@@ -14,26 +14,41 @@
 
 var imageCollection = [];
 var totalClicks = 0;
-var maxClicks = 7;
+var maxClicks = 25; 
+var verifyImage = [];
 
 function DisplayProducts(imageSource, caption){
     this.clicked = 0;
-    // this.shown = 0;
+    this.shown = 0;
     this.imageSrc = imageSource;
     this.imageCaption = caption;
   
     imageCollection.push(this);
 }
     new DisplayProducts('breakfast.jpg', 'breakfast');
-    new DisplayProducts('shark.jpg', 'shark');
-    new DisplayProducts('dragon.jpg', 'dragon meat');
-    new DisplayProducts('cthulhu.jpg', 'chtulhu');
-    new DisplayProducts('tauntaun.jpg', 'tauntaun');
+    new DisplayProducts('shark.jpg', 'Shark');
+    new DisplayProducts('dragon.jpg', 'Dragon Meat');
+    new DisplayProducts('cthulhu.jpg', 'Chtulhu');
+    new DisplayProducts('tauntaun.jpg', 'Tauntaun');
     new DisplayProducts('bag.jpg', 'R2-D2');
-    new DisplayProducts('chair.jpg', 'chair');
-    new DisplayProducts('banana.jpg', 'banana');
-    new DisplayProducts('boots.jpg', 'boots');
-    new DisplayProducts('dog-duck.jpg', 'dog-duck');
+    new DisplayProducts('chair.jpg', 'Chair');
+    new DisplayProducts('banana.jpg', 'Banana');
+    new DisplayProducts('boots.jpg', 'Boots');
+    new DisplayProducts('dog-duck.jpg', 'Dog Duck');
+    new DisplayProducts('bathroom.jpg', 'Bathroom IPAD');
+    new DisplayProducts('bubblegum.jpg', 'Meatball Bubble Gum');
+    new DisplayProducts('pen.jpg', 'Pen');
+    new DisplayProducts('pet-sweep.jpg', 'Pet Sweep');
+    new DisplayProducts('scissors.jpg', 'Pizza Scissors');
+    new DisplayProducts('sweep.png', 'Kid Sweep');
+    new DisplayProducts('unicorn.jpg', 'Unicorn Meat');
+    new DisplayProducts('usb.gif', 'USB');
+    new DisplayProducts('water-can.jpg', 'Water Can');
+    new DisplayProducts('wine-glass.jpg', 'Wine Glass');
+
+
+
+
     
     
     // Set up the event listener
@@ -79,18 +94,18 @@ function rerenderSomeRandomImages(){
     console.log('first new', imageCollection[firstRandom]);
   
     var secondRandom = pickRandom(0, imageCollection.length);
-    console.log('second new', imageCollection[secondRandom]);
+
 
     var thirdRandom = pickRandom(0, imageCollection.length);
-    console.log('third new', imageCollection[thirdRandom]);
   
-    while(secondRandom === firstRandom){
-      secondRandom = pickRandom(0, imageCollection.length);
-      console.log('second new (reroll)', imageCollection[secondRandom]);
-    }
-    while(thirdRandom === firstRandom || thirdRandom === secondRandom){
+    while(secondRandom === firstRandom || thirdRandom === firstRandom || thirdRandom === secondRandom || verifyImage.includes(firstRandom)||verifyImage.includes(secondRandom)|| verifyImage.includes(thirdRandom)){
         thirdRandom = pickRandom(0, imageCollection.length);
+        firstRandom = pickRandom(0, imageCollection.length);
+      secondRandom = pickRandom(0, imageCollection.length);
     }
+    verifyImage[0] = firstRandom;
+    verifyImage[1] = secondRandom;
+    verifyImage[2] = thirdRandom;
     
     var leftImage = document.getElementById('left-image');
     var leftText = document.getElementById('left-text');
@@ -143,3 +158,36 @@ for(var i = 0; i < this.imageCollection.length; i++){
 }
   }
  
+
+
+
+//   var ctx = document.getElementById('myChart').getContext('2d');
+// var chart = new Chart(ctx, {
+//   // The type of chart we want to create
+//   type: 'graph',
+
+//   data: {
+//     labels: ['', 'Breakfast', 'Shark', 'Dragon Meat', 'Chtulhu', 'Tauntaun', 'R2-D2', 'Chair', 'Banana', 'Boots', 'Dog Duck', 'Bathroom IPAD', 'Meatball Bubble Gum', 'Pen', 'Pet Sweep', 'Pizza Scissors', 'Kid Sweep', 'Unicorn Meat', 'USB', 'Water Can', 'Wine Glass'],
+//     datasets: [{
+//       label: 'Which product is best',
+//       backgroundColor: 'orange',
+//       borderColor: 'purple',
+//       data: [{y: 0, x: 9, r:300}, {y: 10, x:20, r:300}, {y:5, x:20, r:300}, {y:2, x: 30, r:300}, {y: 20, x: 5, r:300}, {y:30, x:30, r:300}, {y: 45, x: 45, r:300}, {y:50, x:20, r:600}]
+//     }]
+//   },
+//   // Configuration options go here
+//   options: {
+//     title: {
+//       display: true,
+//       text: 'Custom Chart Title',
+//     },
+//     layout: {
+//       padding: {
+//         left: 50,
+//         right: 0,
+//         top: 0,
+//         bottom: 0
+//       }
+//     }
+//   }
+// });
