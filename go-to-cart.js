4b17bcc645f761008c373a622dc635f3e0fa775a
david <david.c.quinn25>
2020-06-17 16:03:36 -0700

    had to restart again

---
+++ go-to-cart.js
@@ -0,0 +1,14 @@
+function returnTopChoice(){
+    return clicked
+}
+
+var topChoice = imageCollection[0];
+
+for() {
+    topChoice.clicked < imageCollection[i].clicked
+
+
+if 
+}
+//for loop for all values
+//each time have them compare to the other one
